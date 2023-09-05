c8ca87a958b3c8072285d927954be434caf652e0
FukuInTheCode <tomc.code@gmail.com>
2023-09-06 01:18:56 +0200

    hard reset

--- includes/my_viz.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#define MY_VIZ_H
-
-#include "my_plot.h"
-#include "my_neuralnetwork.h"
-
-void my_nn_viz_error(my_nn_t *N, my_plot_t *plt, my_params_t *params, my_matrix_t *features, my_matrix_t *y);
--- src/main.c
+++ src/main.c
@@ -1,24 +1,5 @@
 #include "../includes/my.h"
 
-// AND - gates
-double and_train_fea[] = {
-    0, 0,
-    0, 1,
-    1, 0,
-    1, 1
-};
-double and_train_tar[] = {
-    0,
-    0,
-    0,
-    1
-};
-
-static double fun(double x)
-{
-    return x * 2;
-}
-
 int main(void)
 {
     srand(time(0));
