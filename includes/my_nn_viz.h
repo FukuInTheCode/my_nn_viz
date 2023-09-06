514535e72cf6e369c434d738be64ac80d8682836
FukuInTheCode <tomc.code@gmail.com>
2023-09-06 23:27:27 +0200

    change in header

--- includes/my_nn_viz.h
+++ includes/my_nn_viz.h
@@ -5,5 +5,6 @@
 #include "my_plot.h"
 
 void my_nn_viz_get_error_graph(my_graph_t *g, my_nn_t *nn, my_matrix_t *x,  my_matrix_t *y, my_params_t *hp);
+void my_nn_viz_arch(my_nn_t *nn, sfRenderWindow *window);
 
 #endif
