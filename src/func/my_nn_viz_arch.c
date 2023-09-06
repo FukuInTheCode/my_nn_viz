85fa431546bb25b95691415c684e50af87800002
FukuInTheCode <tomc.code@gmail.com>
2023-09-07 00:18:14 +0200

    changes

--- src/func/my_nn_viz_arch.c
+++ src/func/my_nn_viz_arch.c
@@ -9,48 +9,48 @@ typedef struct {
     my_nn_t *nn;
 } my_shit_t;
 
-static draw_connection(my_shit_t *s, sfRenderWindow *window, double nlayer_vpad, sfVector2f pos)
+static void draw_connection(my_shit_t *s, sfRenderWindow *window,\
+                                double nlayer_vpad, sfVector2f pos)
 {
+    sfColor start = COLOR1_W;
+    sfColor end = COLOR2_W;
     for (uint32_t k = 0; k < s->nn->dims[s->i + 1]; ++k) {
         sfVector2f pos2 = {
             .x = (s->i + 1) * s->layer_hpad + PAD_X + s->radius * 2,
             .y = k * nlayer_vpad + PAD_Y + nlayer_vpad / 2
         };
         sfVertex connection[] = {
-            {pos, interpolate_color(start, end, s->nn->theta_arr[s->i].arr[k][s->j]), {0, 0}},
-            {pos2, interpolate_color(start, end, s->nn->theta_arr[s->i].arr[k][s->j]), {0, 0}}
+            {pos, interpolate_color(start, end,\
+                            s->nn->theta_arr[s->i].arr[k][s->j]), {0, 0}},
+            {pos2, interpolate_color(start, end,\
+                                s->nn->theta_arr[s->i].arr[k][s->j]), {0, 0}}
         };
         sfRenderWindow_drawPrimitives(window, connection, 2, sfLines, NULL);
     }
 }
 
-static void draw_connections(sfRenderWindow *window, my_shit_t *s)
+static void draw_connections(sfRenderWindow *window, my_shit_t *s, sfVector2f pos)
 {
     sfVector2u window_size = sfRenderWindow_getSize(window);
-    sfColor start = COLOR1_W;
-    sfColor end = COLOR2_W;
     if (s->i != s->nn->size - 1) {
-        sfVector2f pos = {
-            .x = s->i * s->layer_hpad + PAD_X + s->radius * 2,
-            .y = s->j * s->layer_vpad + PAD_Y + s->layer_vpad / 2
-        };
         double nlayer_vpad = (window_size.y - PAD_Y * 2) / (double)(s->nn->dims[s->i + 1]);
         draw_connection(s, window, nlayer_vpad, pos);
     }
 }
 
-static void plot_neuron(sfRenderWindow *window, my_shit_t *s, my_nn_t *nn)
+static void plot_neuron(sfRenderWindow *window, my_shit_t *s)
 {
-    sfColor start = COLOR1_B;
-    sfColor end = COLOR2_B;
     sfVector2f pos = {
         .x = s->i * s->layer_hpad + PAD_X + s->radius * 2,
         .y = s->j * s->layer_vpad + PAD_Y + s->layer_vpad / 2
     };
+    draw_connections(window, s, pos);
+    sfColor start = COLOR1_B;
+    sfColor end = COLOR2_B;
     sfColor color = COLOR_0;
     if (s->i != 0)
         color = interpolate_color(start, end,\
-                    nn->bias_arr[s->i - 1].arr[s->j][0]);
+                    s->nn->bias_arr[s->i - 1].arr[s->j][0]);
     pos.x -= s->radius;
     pos.y -= s->radius;
     sfCircleShape *pt = sfCircleShape_create();
@@ -78,8 +78,7 @@ void my_nn_viz_arch(my_nn_t *nn, sfRenderWindow *window)
                 .j = j,
                 .nn = nn
             };
-            draw_connections(window, &s);
-            plot_neuron(window, &s, nn);
+            plot_neuron(window, &s);
         }
     }
 }
