5562d430d31d70c13f3a3f961de7a8fa12e99842
FukuInTheCode <tomc.code@gmail.com>
2023-09-06 23:27:03 +0200

    move the arch plotiçng to its own files

--- src/main.c
+++ src/main.c
@@ -152,12 +152,6 @@ int main(void)
 
     my_nn_create(&neuro);
 
-
-    sfColor start = {0, 255, 0, 255};
-    sfColor end = {255, 0, 255, 255};
-    sfColor start_w = {0, 255, 255, 0};
-    sfColor end_w = {255, 0, 0, 255};
-
     sfVideoMode mode = {1000, 1000, 32};
     sfRenderWindow *window = sfRenderWindow_create(mode, "test", sfDefaultStyle, NULL);
 
@@ -172,9 +166,6 @@ int main(void)
         .y = 50
     };
 
-    double layer_hpad = (window_size.x - padding.x * 2) / (double)neuro.size;
-
-    double radius = 1. / 4. * layer_hpad;
     uint32_t h = 0;
     // show loop
     while (sfRenderWindow_isOpen(window)) {
@@ -192,44 +183,7 @@ int main(void)
         // ploting
         sfRenderWindow_clear(window, sfBlack);
         // loop for each layer
-        for (uint32_t i = 0; i < neuro.size; ++i) {
-            double layer_vpad = (window_size.y - padding.y * 2) / (double)(neuro.dims[i]);
-            // for each neuro of each layer
-            for (uint32_t j = 0; j < neuro.dims[i]; ++j) {
-                sfVector2f pos = {
-                    .x = i * layer_hpad + padding.x + radius * 2,
-                    .y = j * layer_vpad + padding.y + layer_vpad / 2
-                };
-                if (i != neuro.size - 1) {
-                    double nlayer_vpad = (window_size.y - padding.y * 2) / (double)(neuro.dims[i + 1]);
-                    // for each next neuron
-                    for (uint32_t k = 0; k < neuro.dims[i + 1]; ++k) {
-                        sfVector2f pos2 = {
-                            .x = (i + 1) * layer_hpad + padding.x + radius * 2,
-                            .y = k * nlayer_vpad + padding.y + nlayer_vpad / 2
-                        };
-                        sfVertex connection[] = {
-                            {pos, interpolateColor(start_w, end_w, neuro.theta_arr[i].arr[k][j]), {0, 0}},
-                            {pos2, interpolateColor(start_w, end_w, neuro.theta_arr[i].arr[k][j]), {0, 0}}
-                        };
-                        sfRenderWindow_drawPrimitives(window, connection, 2, sfLines, NULL);
 
-                    }
-                }
-                pos.x -= radius;
-                pos.y -= radius;
-                sfCircleShape *pt = sfCircleShape_create();
-                if (i == 0)
-                    sfCircleShape_setFillColor(pt, sfRed);
-                else
-                    sfCircleShape_setFillColor(pt, interpolateColor(start, end, neuro.bias_arr[i - 1].arr[j][0]));
-
-                sfCircleShape_setRadius(pt, radius);
-                sfCircleShape_setPosition(pt, pos);
-                sfRenderWindow_drawCircleShape(window, pt, NULL);
-                sfCircleShape_destroy(pt);
-            }
-        }
         // end ploting
 
         sfRenderWindow_display(window);
