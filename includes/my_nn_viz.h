81d476c74b0cf3d1f49886e275d9f8750691c095
FukuInTheCode <tomc.code@gmail.com>
2023-09-06 23:41:52 +0200

    changes

--- includes/my_nn_viz.h
+++ includes/my_nn_viz.h
@@ -10,6 +10,7 @@
 #define COLOR2_W {255, 0, 0, 255}
 #define COLOR1_B {0, 255, 0, 255}
 #define COLOR2_B {255, 0, 255, 255}
+#define COLOR_0 sfRed
 
 void my_nn_viz_get_error_graph(my_graph_t *g, my_nn_t *nn, my_matrix_t *x,  my_matrix_t *y, my_params_t *hp);
 void my_nn_viz_arch(my_nn_t *nn, sfRenderWindow *window);
--- src/func/my_nn_viz_arch.c
+++ src/func/my_nn_viz_arch.c
@@ -2,21 +2,19 @@
 
 static void draw_connections(my_nn_t *nn, sfRenderWindow *window, uint32_t i, uint32_t j)
 {
-    sfVector2f padding = {
-        .x = 50,
-        .y = 50
-    };
+    sfColor start = COLOR1_W;
+    sfColor end = COLOR2_W;
     if (i != nn->size - 1) {
         double nlayer_vpad = (window_size.y - PAD_Y * 2) / (double)(nn->dims[i + 1]);
         // for each next nn->
         for (uint32_t k = 0; k < nn->dims[i + 1]; ++k) {
             sfVector2f pos2 = {
-                .x = (i + 1) * layer_hpad + padding.x + radius * 2,
-                .y = k * nlayer_vpad + padding.y + nlayer_vpad / 2
+                .x = (i + 1) * layer_hpad + PAD_X + radius * 2,
+                .y = k * nlayer_vpad + PAD_Y + nlayer_vpad / 2
             };
             sfVertex connection[] = {
-                {pos, interpolate_color(start_w, end_w, nn->theta_arr[i].arr[k][j]), {0, 0}},
-                {pos2, interpolate_color(start_w, end_w, nn->theta_arr[i].arr[k][j]), {0, 0}}
+                {pos, interpolate_color(start, end, nn->theta_arr[i].arr[k][j]), {0, 0}},
+                {pos2, interpolate_color(start, end, nn->theta_arr[i].arr[k][j]), {0, 0}}
             };
             sfRenderWindow_drawPrimitives(window, connection, 2, sfLines, NULL);
         }
@@ -25,17 +23,18 @@ static void draw_connections(my_nn_t *nn, sfRenderWindow *window, uint32_t i, ui
 
 void my_nn_viz_arch(my_nn_t *nn, sfRenderWindow *window)
 {
+    sfColor start = COLOR1_B;
+    sfColor end = COLOR2_B;
     sfVector2u window_size = sfRenderWindow_getSize(window);
     double layer_hpad = (window_size.x - padding.x * 2) / (double)nn->size;
     double radius = 1. / 4. * layer_hpad;
-
     for (uint32_t i = 0; i < nn->size; ++i) {
         double layer_vpad = (window_size.y - padding.y * 2) / (double)(nn->dims[i]);
         // for each nn->of each layer
         for (uint32_t j = 0; j < nn->dims[i]; ++j) {
             sfVector2f pos = {
-                .x = i * layer_hpad + padding.x + radius * 2,
-                .y = j * layer_vpad + padding.y + layer_vpad / 2
+                .x = i * layer_hpad + PAD_X + radius * 2,
+                .y = j * layer_vpad + PAD_Y + layer_vpad / 2
             };
 
             draw_connections(nn, window, i, j);
@@ -45,7 +44,7 @@ void my_nn_viz_arch(my_nn_t *nn, sfRenderWindow *window)
             if (i == 0)
                 sfCircleShape_setFillColor(pt, sfRed);
             else
-                sfCircleShape_setFillColor(pt, interpolate_color(start, end, nn->bias_arr[i - 1].arr[j][0]));
+                sfCircleShape_setFillColor(pt, interpolate_color(start, ends, nn->bias_arr[i - 1].arr[j][0]));
             sfCircleShape_setRadius(pt, radius);
             sfCircleShape_setPosition(pt, pos);
             sfRenderWindow_drawCircleShape(window, pt, NULL);
