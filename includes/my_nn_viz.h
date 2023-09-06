1ac11f6d9929faa89c8097f94c86b5d08f3a73bd
FukuInTheCode <tomc.code@gmail.com>
2023-09-07 00:22:02 +0200

    chaneg in h

--- includes/my_nn_viz.h
+++ includes/my_nn_viz.h
@@ -14,5 +14,6 @@
 
 void my_nn_viz_get_error_graph(my_graph_t *g, my_nn_t *nn, my_matrix_t *x,  my_matrix_t *y, my_params_t *hp);
 void my_nn_viz_arch(my_nn_t *nn, sfRenderWindow *window);
+void my_nn_viz_repr(my_nn_t *nn, sfVideoMode mode);
 
 #endif
