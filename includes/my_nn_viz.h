c73d84eb4bbe4b822a80a3b81fa349eec8769176
FukuInTheCode <tomc.code@gmail.com>
2023-09-06 23:38:40 +0200

    define some macro to change the graphic

--- includes/my_nn_viz.h
+++ includes/my_nn_viz.h
@@ -4,6 +4,13 @@
 #include "my_neuralnetwork.h"
 #include "my_plot.h"
 
+#define PAD_X 50
+#define PAD_Y 50
+#define COLOR1_W {0, 255, 255, 255}
+#define COLOR2_W {255, 0, 0, 255}
+#define COLOR1_B {0, 255, 0, 255}
+#define COLOR2_B {255, 0, 255, 255}
+
 void my_nn_viz_get_error_graph(my_graph_t *g, my_nn_t *nn, my_matrix_t *x,  my_matrix_t *y, my_params_t *hp);
 void my_nn_viz_arch(my_nn_t *nn, sfRenderWindow *window);
 
